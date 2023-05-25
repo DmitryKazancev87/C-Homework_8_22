99db85f00f07fc3adb79b1e00d02568b7179c58b
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-05-25 21:12:10 +0300

    Решение задачи 22 (54 загрузка повторно)

---
+++ Task 22/Program.cs
@@ -0,0 +1,53 @@
+﻿// 22. Программа, которая задает двумерный массив и сортирует по убыванию элементы каждой
+// строки двумерного массива.
+
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void SortArray(int[,] array)
+
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] < array[i, k])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} | ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] array = new int[3, 4];
+CreateArray(array);
+SortArray(array);
+PrintArray(array);
+Console.WriteLine("Строки двумерного массива отсортированы по убыванию.");
